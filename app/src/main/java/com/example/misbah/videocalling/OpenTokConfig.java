70128dab952c63d79e79a50dbdc8abee5e7e1834
zapyle-ramky <ramky@zapyle.com>
2016-12-29 11:17:24 +0530

    first commit

---
+++ app/src/main/java/com/example/misbah/videocalling/OpenTokConfig.java
@@ -0,0 +1,18 @@
+package com.example.misbah.videocalling;
+
+/**
+ * Created by Misbah on 28/12/16.
+ */
+public class OpenTokConfig {
+
+
+    public static final String API_KEY ="45740662";
+
+    public static final String SESSION_ID ="1_MX40NTc0MDY2Mn5-MTQ4Mjk1MTcyNTgwMn5MUWhINGYwV0hERTRKTUV2cEpFMDFxdU5-fg";
+
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTc0MDY2MiZzaWc9NzZmNDliNjk1YWQwZjQwYWU0MWY4MTQ5MDY3MWU0Yjg5NDE0MmEwNTpzZXNzaW9uX2lkPTFfTVg0ME5UYzBNRFkyTW41LU1UUTRNamsxTVRjeU5UZ3dNbjVNVVdoSU5HWXdWMGhFUlRSS1RVVjJjRXBGTURGeGRVNS1mZyZjcmVhdGVfdGltZT0xNDgyOTUxOTkxJm5vbmNlPTAuMTY2MjQ5MzQxMzI4NTM0NDImcm9sZT1tb2RlcmF0b3ImZXhwaXJlX3RpbWU9MTQ4MzAzODM5MA==";
+
+
+    public static final boolean SUBSCRIBE_TO_SELF = true;
+
+}
